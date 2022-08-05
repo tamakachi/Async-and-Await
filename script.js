d62313dc0a1c5fa613d34a39e12b8dcbfea6d55f
Kent <kenttechnical@gmail.com>
2022-08-05 12:35:14 +0200

    final

--- script.js
+++ script.js
@@ -4,11 +4,16 @@
 const getToDos = async () => {
 
     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    
+    if (response.status !== 200){
+        throw new Error('Cannot fetch data')
+    }
+    
     const data = await response.json()
     return data
 
 }
 
-getToDos().then((data)=>{
-    console.log(data)
-})
+getToDos()
+    .then((data)=>{console.log(data)})
+    .catch((err)=> {console.log(err.message)})
